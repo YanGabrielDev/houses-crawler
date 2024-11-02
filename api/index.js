a30bf8e90253e251b77b5d644e494932b0c5bd10
Yan_Gabriel <yangabriel2012@gmail.com>
2024-11-02 18:05:12 -0400

    adicionando listen para funcionar a api localmente

--- api/index.js
+++ api/index.js
@@ -1,17 +1,22 @@
 import Fastify from 'fastify'
-import { housesCrawler } from '../src/services/quinto-andar-service.js'
-
+import dotenv from 'dotenv'
+import housesController from '../src/controllers/houses-controller.js'
+dotenv.config()
 const app = Fastify({
   logger: true,
 })
 
-app.get('/', async (req, reply) => {
-  const complete = await housesCrawler()
-  return reply.status(200).send({ complete })
+app.get('/', housesController.houses)
+
+// export default async function handler(req, reply) {
+//   await app.ready()
+//   app.server.emit('request', req, reply)
+// }
+
+app.listen({ port: 3000 }, function (err, address) {
+  if (err) {
+    app.log.error(err)
+    process.exit(1)
+  }
+  // Server is now listening on ${address}
 })
-
-export default async function handler(req, reply) {
-  await app.ready()
-  app.server.emit('request', req, reply)
-}
-
