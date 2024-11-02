fd938d3eb1a1ba8e3b01f855825ab9380b4f0173
Yan_Gabriel <yangabriel2012@gmail.com>
2024-11-02 18:09:10 -0400

    criando turso client

---
+++ src/db/tursoClient.js
@@ -0,0 +1,30 @@
+import { createClient } from "@libsql/client";
+
+// Criação do cliente Turso
+export const turso = createClient({
+    url: process.env.TURSO_DATABASE_URL,
+    authToken: process.env.TURSO_AUTH_TOKEN,
+});
+
+
+// Função para criar a tabela "houses"
+export async function createHousesTable() {
+    try {
+        await turso.execute(`
+      CREATE TABLE IF NOT EXISTS houses (
+        id INT,
+        type VARCHAR(255),
+        area INT,
+        bedrooms INT,
+        salePrice INT,
+        address VARCHAR(255),
+        city VARCHAR(255),
+        neighbourhood VARCHAR(255),
+        regionName VARCHAR(255)
+      )
+    `);
+        console.log("Tabela 'houses' criada com sucesso!");
+    } catch (error) {
+        console.error("Erro ao criar a tabela 'houses':", error);
+    }
+}
