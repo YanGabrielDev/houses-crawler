005e58d47ab00cc95d1d8323181fbf68a617010c
Yan_Gabriel <yangabriel2012@gmail.com>
2024-11-20 13:03:21 -0500

    adicionando

--- api/index.js
+++ api/index.js
@@ -8,15 +8,15 @@ const app = Fastify({
 
 app.get('/', housesController.houses)
 
-export default async function handler(req, reply) {
-  await app.ready()
-  app.server.emit('request', req, reply)
-}
+// export default async function handler(req, reply) {
+//   await app.ready()
+//   app.server.emit('request', req, reply)
+// }
 
-// app.listen({ port: 3000 }, function (err, address) {
-//   if (err) {
-//     app.log.error(err)
-//     process.exit(1)
-//   }
-//   // Server is now listening on ${address}
-// })
+app.listen({ port: 3000 }, function (err, address) {
+  if (err) {
+    app.log.error(err)
+    process.exit(1)
+  }
+  // Server is now listening on ${address}
+})
--- cron/index.js
+++ cron/index.js
@@ -1,3 +1,5 @@
 import { housesCrawler } from "../src/services/quinto-andar-service.js";
+import dotenv from 'dotenv'
 
+dotenv.config()
 housesCrawler()
